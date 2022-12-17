77983abbe63781e7f7fe636e21e28c1b87f68953
Marzz <mariamadesina29@gmail.com>
2022-12-17 21:39:53 +0100

    exploratory data analysis in sql

--- Hotel-Bookings-Analysis/HotelBookings.sql
+++ Hotel-Bookings-Analysis/HotelBookings.sql
@@ -26,4 +26,24 @@ FROM dbo.[2019Booking]
 UNION
 SELECT *
 FROM dbo.[2020Booking])
-
+/* Use the distinct function to explore the unique values in each column*/
+--SELECT DISTINCT hotel 
+--SELECT DISTINCT is_canceled
+--SELECT DISTINCT meal
+--SELECT DISTINCT country
+--SELECT DISTINCT market_segment
+SELECT DISTINCT distribution_channel
+--SELECT DISTINCT is_repeated_guest
+--SELECT DISTINCT previous_cancellations
+--SELECT DISTINCT previous_bookings_not_canceled
+--SELECT DISTINCT reserved_room_type
+--SELECT DISTINCT assigned_room_type
+--SELECT DISTINCT booking_changes
+--SELECT DISTINCT deposit_type
+--SELECT DISTINCT agent
+--SELECT DISTINCT company
+--SELECT DISTINCT customer_type
+--SELECT DISTINCT required_car_parking_spaces
+--SELECT DISTINCT total_of_special_requests
+--SELECT DISTINCT reservation_status
+FROM Hotels
